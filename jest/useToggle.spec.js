3dae6ccb3cc8a0f97f0eb2453e8b14b45d7b2836
衣沾不足惜 <javascript.h@qq.com>
2020-09-12 11:25:03 +0800

    🧪test(toggle): add unit test
    Signed-off-by: 衣沾不足惜 <javascript.h@qq.com>

--- jest/useToggle.spec.js
+++ jest/useToggle.spec.js
@@ -1,14 +1,79 @@
-import { shallowMount } from '@vue/test-utils';
+import { mount, shallowMount } from '@vue/test-utils';
 
-import useToggle from '@lib/useToggle';
-describe('test useToggle', () => {
-  it('renders a message', () => {
-    const wrapper = shallowMount(useToggle, {
-      propsData: {
-        message: 'Testing the composition API'
-      }
-    });
+import { useToggle } from '@/index.js';
 
-    expect(wrapper.find('.message').text()).toBe('TESTING THE COMPOSITION API');
-  });
+function forCallback (count, callback) {
+  for (let i = 0; i < count; i++) {
+    callback();
+  }
+}
+function for101 (callback) {
+  for (let i = 0; i < 101; i++) {
+    callback();
+  }
+}
+test('init 1  equals true', () => {
+  const [state, toggle] = useToggle(1);
+  expect(state.value).toBe(true);
+  toggle();
+  expect(state.value).toBe(false);
+  forCallback(100, toggle);
+  expect(state.value).toBe(false);
+  forCallback(101, toggle);
+  expect(state.value).toBe(true);
+});
+
+test('init undefined  equals false', () => {
+  const [state] = useToggle();
+  expect(state.value).toBe(false);
+});
+
+test('init []  equals false', () => {
+  const [state] = useToggle();
+  expect(state.value).toBe(false);
+});
+
+test('init blue , red . 预期对调', () => {
+  const [state, toggle] = useToggle('blue', 'red');
+  expect(state.value).toBe('blue');
+  toggle();
+  expect(state.value).toBe('red');
+  toggle();
+  expect(state.value).toBe('blue');
+  forCallback(100, toggle);
+  expect(state.value).toBe('blue');
+  forCallback(101, toggle);
+  expect(state.value).toBe('red');
+});
+
+test('init [blue,red]. 预期对调', () => {
+  const [state, toggle] = useToggle(['blue', 'red']);
+  expect(state.value).toBe('blue');
+  toggle();
+  expect(state.value).toBe('red');
+  toggle();
+  expect(state.value).toBe('blue');
+  forCallback(100, toggle);
+  expect(state.value).toBe('blue');
+  forCallback(101, toggle);
+  expect(state.value).toBe('red');
+});
+
+test('长数组 预期顺序轮换', () => {
+  let [state, toggle] = useToggle(['blue', 'red', 'green', 'error', 'warn']);
+  expect(state.value).toBe('blue');
+  toggle();
+  expect(state.value).toBe('red');
+  forCallback(3, toggle);
+  expect(state.value).toBe('warn'); // loop end
+  forCallback(18, toggle); // 18 % 4 == 2
+  expect(state.value).toBe('green');
+  forCallback(3, toggle);
+  expect(state.value).toBe('blue');
+});
+
+test('useToggle 返回值 只读', () => {
+  let [state] = useToggle(['blue', 'red']);
+  state.value = 'yellow'; // 发出警告
+  expect(state.value).toBe('blue');
 });
