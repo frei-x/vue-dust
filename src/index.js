fa71e7ebb9a036a866993dbbf5564df3fee8e5f3
衣沾不足惜 <javascript.h@qq.com>
2020-09-13 13:26:15 +0800

    ✨feat(useState):  可记录state 历史
    Signed-off-by: 衣沾不足惜 <javascript.h@qq.com>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { useCrud } from '@lib/useCrud';
 export { useToggle } from '@lib/useToggle';
 export { useWatchDom } from '@lib/useWatchDom';
+export { useState } from '@lib/useState';
--- vue.config.js
+++ vue.config.js
@@ -8,15 +8,15 @@ module.exports = {
         '@lib': '@/composition-lib',
         '@utils': '@/utils',
         // https://github.com/facebook/react/issues/13991 使用项目中的vue ,否则多个 视图不刷新
-        vue: path.resolve('./node_modules/vue'),
-      },
-    },
+        vue: path.resolve('./node_modules/vue')
+      }
+    }
   },
   // 删除 HTML 相关的 webpack 插件
-  chainWebpack: (config) => {
+  chainWebpack: config => {
     config.plugins.delete('html');
     config.plugins.delete('preload');
     config.plugins.delete('prefetch');
   },
-  outputDir: 'dist',
+  outputDir: 'dist'
 };
