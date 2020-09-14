1234569abce30da2206510feb323528294ea207b
衣沾不足惜 <javascript.h@qq.com>
2020-09-15 00:29:02 +0800

    🔧build: 点选 /  空值表达式
    Signed-off-by: 衣沾不足惜 <javascript.h@qq.com>

--- babel.config.js
+++ babel.config.js
@@ -4,5 +4,7 @@ module.exports = {
     test: {
       presets: [['@babel/preset-env', { targets: { node: 'current' } }]]
     }
-  }
+  },
+  // ?? .?
+  plugins: ['@babel/plugin-proposal-nullish-coalescing-operator', '@babel/plugin-proposal-optional-chaining']
 };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 export { useToggle } from '@lib/useToggle';
 export { useWatchDom } from '@lib/useWatchDom';
 export { useState } from '@lib/useState';
+
+const obj = {
+  test: null,
+  color: 'red'
+};
+
+console.log(obj.color?.a?.cc ?? '默认值');
+console.log(obj.test ?? 1);
